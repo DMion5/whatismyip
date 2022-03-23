4e9c3c79927e9182fb4bf37a7c5d9105b56c0ecb
Will Whitaker <will.whitaker@unc.edu>
2022-03-23 17:03:34 -0400

    gunicorn config

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
+"""
+WSGI config for project
 
-import sys
-import site
+"""
 
-site.addsitedir('/var/www/hitme/lib/python3.6/site-packages')
+#import sys
+#import site
 
-sys.path.insert(0, '/var/www/hitme')
+#site.addsitedir('/var/www/hitme/lib/python3.6/site-packages')
+
+#sys.path.insert(0, '/var/www/hitme')
 
 from app import app as application
+
+if __name__ == "__main__":
+    application.run()

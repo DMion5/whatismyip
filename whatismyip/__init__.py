57a07bb754d3a8ddbcfd3da9c7e8fa939b0f9f28
Will Whitaker <will.whitaker@unc.edu>
2022-08-19 11:53:08 -0400

    debug

--- whatismyip/__init__.py
+++ whatismyip/__init__.py
@@ -60,6 +60,7 @@ def home():
     #context['client_address'] = '152.23.198.240'
     #context['client_address'] = '172.17.32.38'
     #context['client_address'] = '75.183.206.183'
+    #context['client_address'] = '198.85.230.11'
     #context['client_address'] = '2610:28:3091:1000:2::a'
     #context['client_address'] = '2610:28:3090:1000::d6:e1'
     #context['client_address'] = '2603:6081:7041:8101:cd13:7d19:ae:20ed'
